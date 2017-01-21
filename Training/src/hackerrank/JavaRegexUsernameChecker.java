17f7b853006de9f8554d052254036d18c72df15f
ibesli <ibesli@192.168.1.24>
2017-01-21 08:29:48 +0200

    Hackerrank Regex Username Checker

---
+++ Training/src/hackerrank/JavaRegexUsernameChecker.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaRegexUsernameChecker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while (testCases > 0) {
+			String username = in.nextLine();
+			// String pattern = Complete this line in the editable area below
+			String pattern = "^[a-zA-Z]{1}[\\w]{7,29}$";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(username);
+
+			if (m.find()) {
+				System.out.println("Valid");
+			} else {
+				System.out.println("Invalid");
+			}
+			testCases--;
+		}
+	}
+}
