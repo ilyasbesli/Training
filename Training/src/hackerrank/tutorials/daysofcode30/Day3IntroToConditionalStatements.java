16055294b2723a6022ff38d810435d99a8025851
ibesli <ibesli@192.168.1.24>
2017-02-03 22:05:22 +0200

    Day 3: Intro to Conditional Statements

---
+++ Training/src/hackerrank/tutorials/daysofcode30/Day3IntroToConditionalStatements.java
@@ -0,0 +1,69 @@
+package hackerrank.tutorials.daysofcode30;
+
+import java.util.Scanner;
+
+/**
+ * Day 3: Intro to Conditional Statements
+ * 
+ * Objective In this challenge, we're getting started with conditional
+ * statements. Check out the Tutorial tab for learning materials and an
+ * instructional video!
+ * 
+ * Task Given an integer, , perform the following conditional actions:
+ * 
+ * If is odd, print Weird If is even and in the inclusive range of to , print
+ * Not Weird If is even and in the inclusive range of to , print Weird If is
+ * even and greater than , print Not Weird Complete the stub code provided in
+ * your editor to print whether or not is weird.
+ * 
+ * Input Format
+ * 
+ * A single line containing a positive integer, .
+ * 
+ * Constraints
+ * 
+ * Output Format
+ * 
+ * Print Weird if the number is weird; otherwise, print Not Weird.
+ * 
+ * Sample Input 0
+ * 
+ * 3 Sample Output 0
+ * 
+ * Weird Sample Input 1
+ * 
+ * 24 Sample Output 1
+ * 
+ * Not Weird Explanation
+ * 
+ * Sample Case 0: is odd and odd numbers are weird, so we print Weird.
+ * 
+ * Sample Case 1: and is even, so it isn't weird. Thus, we print Not Weird.
+ * 
+ * 
+ * @author ibesli
+ *
+ */
+public class Day3IntroToConditionalStatements {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		String ans = "";
+
+		// if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+		if (n % 2 == 1) {
+			ans = "Weird";
+		} else {
+			// Complete the code
+			if (n >= 2 && n <= 5)
+				ans = "Not Weird";
+			else if (n >= 6 && n <= 20)
+				ans = "Weird";
+			else
+				ans = "Not Weird";
+		}
+		System.out.println(ans);
+	}
+}
