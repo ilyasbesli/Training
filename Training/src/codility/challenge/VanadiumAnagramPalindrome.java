4f6fa3a7a6dbaf2484811ee9efc6f2f881045190
ibesli <ibesli@MacbookPros-MacBook-Pro.local>
2017-03-09 22:47:05 +0300

    Vanadium challenge, will continue

---
+++ Training/src/codility/challenge/VanadiumAnagramPalindrome.java
@@ -0,0 +1,15 @@
+package codility.challenge;
+
+public class VanadiumAnagramPalindrome {
+
+	public static void main(String[] args) {
+		VanadiumAnagramPalindrome vanadiumAnagramPalindrome = new VanadiumAnagramPalindrome();
+		vanadiumAnagramPalindrome.solution("0200202002");
+	}
+
+	public int solution(String S) {
+		// secilen kumede bulunan rakamlarin unique olarak toplamlari alinir. Tek sayida olan en fazla 1 tane olabilir
+		return 0;
+	}
+
+}
